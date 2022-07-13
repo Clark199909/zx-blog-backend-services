1494ae52c8a379d0c99046f237aca07bc37ff0e8
Zhengkai Xie <xiezhengkai199982@gmail.com>
2022-07-13 00:23:33 -0400

    add validation

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email(message = "Email format is not correct")
     private String email;
+
+    @NotEmpty(message = "Comment body should not be empty")
+    @Size(min = 10, message = "Comment body should be at least 10 characters")
     private String body;
 }
